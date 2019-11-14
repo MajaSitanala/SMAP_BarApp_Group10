143cb40d7d01bc0df46083bdd93057623ba6aba3
MajaSitanala <maja.sitanala@gmail.com>
2019-11-14 13:49:33 +0100

    Added picture

---
+++ RUS1_Bar/app/src/main/java/com/example/rus1_bar/BarLogin.java
@@ -0,0 +1,14 @@
+package com.example.rus1_bar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BarLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_login);
+    }
+}
