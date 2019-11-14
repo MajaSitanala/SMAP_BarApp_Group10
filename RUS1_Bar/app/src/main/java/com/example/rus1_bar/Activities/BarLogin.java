1b82de649d6d610c9600997abb9892dc0c22da57
MajaSitanala <maja.sitanala@gmail.com>
2019-11-14 13:56:20 +0100

    Added icon and folders for java files

--- RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/BarLogin.java
+++ RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/BarLogin.java
@@ -1,14 +1,16 @@
-package com.example.rus1_bar;
+package com.example.rus1_bar.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.rus1_bar.R;
+
+public class BarLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bar_login);
     }
 }
--- RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/MainActivity.java
+++ RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/MainActivity.java
@@ -1,14 +1,16 @@
-package com.example.rus1_bar;
+package com.example.rus1_bar.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BarLogin extends AppCompatActivity {
+import com.example.rus1_bar.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bar_login);
+        setContentView(R.layout.activity_main);
     }
 }
