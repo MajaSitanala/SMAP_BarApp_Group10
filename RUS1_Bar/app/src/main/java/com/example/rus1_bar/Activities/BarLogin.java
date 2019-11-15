1c0d48d91ba88d93c063a2f283dffcf179c9943e
ChrisBroberg <Christoffer.broberg@hotmail.com>
2019-11-15 22:33:27 +0100

    Added Login and integrated firebase

--- RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/BarLogin.java
+++ RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/BarLogin.java
@@ -3,14 +3,26 @@ package com.example.rus1_bar.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.rus1_bar.R;
 
 public class BarLogin extends AppCompatActivity {
 
+    Button loginbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_login);
+
+        loginbtn.findViewById(R.id.LoginBTN);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/MainActivity.java
+++ RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/MainActivity.java
@@ -3,14 +3,29 @@ package com.example.rus1_bar.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.rus1_bar.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Toast toast = Toast.makeText(getApplicationContext(),currentUser.getDisplayName(),Toast.LENGTH_LONG);
     }
 }
