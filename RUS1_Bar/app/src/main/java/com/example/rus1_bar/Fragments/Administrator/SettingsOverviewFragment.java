61d8f9e492ca5e2b9bbd747751f3d0eac4029045
MikkelBleeg <mikkelbleeg@hotmail.com>
2019-11-21 16:01:54 +0100

    Added Guidelines to the already made views, so they will scale to larger items - Also added button clicks to Settings overview except logoff

--- RUS1_Bar/app/src/main/java/com/example/rus1_bar/Fragments/Administrator/SettingsOverviewFragment.java
+++ RUS1_Bar/app/src/main/java/com/example/rus1_bar/Fragments/Administrator/SettingsOverviewFragment.java
@@ -3,11 +3,15 @@ package com.example.rus1_bar.Fragments.Administrator;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.rus1_bar.R;
 
@@ -16,6 +20,11 @@ import com.example.rus1_bar.R;
  */
 public class SettingsOverviewFragment extends Fragment {
 
+    Button tutorSettingsBtn;
+    Button productSettingsBtn;
+    Button rusturSettingsBtn;
+    Button billSettingsBtn;
+    Button logOffBtn;
 
     public SettingsOverviewFragment() {
         // Required empty public constructor
@@ -23,10 +32,37 @@ public class SettingsOverviewFragment extends Fragment {
 
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tutorSettingsBtn = view.findViewById(R.id.tutorSettingsBtn);
+        productSettingsBtn = view.findViewById(R.id.productSettingsBtn);
+        rusturSettingsBtn = view.findViewById(R.id.rusturSettingsBtn);
+        billSettingsBtn = view.findViewById(R.id.billSettingsBtn);
+        logOffBtn = view.findViewById(R.id.logOffBtn);
+
+        View.OnClickListener tutorSettingsClick = Navigation.createNavigateOnClickListener(R.id.action_settingsOverviewFragment_to_tutorSettingsFragment);
+        tutorSettingsBtn.setOnClickListener(tutorSettingsClick);
+
+        View.OnClickListener productSettingsClick = Navigation.createNavigateOnClickListener(R.id.action_settingsOverviewFragment_to_productSettingsFragment);
+        productSettingsBtn.setOnClickListener(productSettingsClick);
+
+        View.OnClickListener rusturSettingsClick = Navigation.createNavigateOnClickListener(R.id.action_settingsOverviewFragment_to_rusturSettingsFragment);
+        rusturSettingsBtn.setOnClickListener(rusturSettingsClick);
+
+        View.OnClickListener billSettingsClick = Navigation.createNavigateOnClickListener(R.id.action_settingsOverviewFragment_to_billSettingsFragment);
+        billSettingsBtn.setOnClickListener(billSettingsClick);
+
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings_overview, container, false);
     }
+
+
 
 }
