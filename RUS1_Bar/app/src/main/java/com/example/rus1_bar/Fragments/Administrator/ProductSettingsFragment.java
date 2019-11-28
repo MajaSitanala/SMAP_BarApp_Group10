5bb1df6a938df2a3dd718ce7b348182a6a6b0185
MikkelBleeg <mikkelbleeg@hotmail.com>
2019-11-28 15:25:36 +0100

    Setting views should be completed. Need to make the fragements for them to navigate further, also the RecycleView needs to be changed, so it won't have the same functionality as the bar view (Temporary display fix)

--- RUS1_Bar/app/src/main/java/com/example/rus1_bar/Fragments/Administrator/ProductSettingsFragment.java
+++ RUS1_Bar/app/src/main/java/com/example/rus1_bar/Fragments/Administrator/ProductSettingsFragment.java
@@ -4,17 +4,37 @@ package com.example.rus1_bar.Fragments.Administrator;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.rus1_bar.Adapters.ProductRecyclerAdapter;
+import com.example.rus1_bar.Models.Product;
 import com.example.rus1_bar.R;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductSettingsFragment extends Fragment {
+
+    private List<Product> testProductList = new ArrayList<>();
+
+    Button addProductBtn;
+    Button editProductBtn;
+    Button addCategoryBtn;
+    Button editCategoryBtn;
+
+    RecyclerView productRecyclerView;
+    RecyclerView.Adapter productRecyclerAdapter;
+    RecyclerView.LayoutManager productLayoutManager;
 
 
     public ProductSettingsFragment() {
@@ -26,7 +46,37 @@ public class ProductSettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_settings, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_product_settings, container, false);
+
+
+        //Test data for the card view
+        fillTestShoppingCardList();
+
+        // Recycler View setup
+        productRecyclerView = rootView.findViewById(R.id.productSettingsRecycleView);
+
+        //Creates the grid layout
+        productLayoutManager = new GridLayoutManager(getActivity(), 1);                                                                //https://youtu.be/SD2t75T5RdY?t=1302
+        productRecyclerView.setLayoutManager(productLayoutManager);
+
+        //Recycler adapter setup
+        productRecyclerAdapter = new ProductRecyclerAdapter(getActivity(), testProductList);
+
+        productRecyclerView.setAdapter(productRecyclerAdapter);
+
+
+        // Inflate the layout for this fragment
+        return rootView; // inflater.inflate(R.layout.fragment_view_tutors, container, false);
     }
 
+    private void fillTestShoppingCardList()
+    {
+        for (int i = 0; i<4; i++)
+        {
+            //Test data for the card view
+            testProductList.add(new Product(i,i, "Gin Hass", 20, R.drawable.ginhass));
+            testProductList.add(new Product(i,i, "Filur", 20, R.drawable.filur));
+            testProductList.add(new Product(i,i, "White Russian", 20, R.drawable.whiterussianlang));
+        }
+    }
 }
