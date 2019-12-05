286cf412c930b49197365d57714c96445adfb958
Andreas Blaabjerg <Andreas.blaabjerg1@hotmail.com>
2019-12-05 15:22:27 +0100

    Appbar item name changing from Login to Tutors for fragment access between actions. Added resource externalization and translations

--- RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/MainActivity.java
+++ RUS1_Bar/app/src/main/java/com/example/rus1_bar/Activities/MainActivity.java
@@ -20,15 +20,23 @@ import com.google.firebase.auth.FirebaseUser;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    public boolean loggedIn = false;
+
     private FirebaseAuth mAuth;
+    NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.MAtoolbar);
         setSupportActionBar(myToolbar);
+
+        navController = Navigation.findNavController((this), R.id.nav_host_fragment);
+
+
 
         // Initialize Firebase Auth
        mAuth = FirebaseAuth.getInstance();
@@ -45,20 +53,43 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_login:
+        boolean titleFlag = true;
 
-                //Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_LONG).show();
 
-                // Two versions of shifting from tutorViewFragment to Loginfragment - couldnt workaround the first one - next one works.
-                //Navigation.findNavController(findViewById(R.id.MainActivity)).navigate(R.id.action_viewTutorsFragment_to_loginFragment);
+        switch (item.getTitle().toString()) {
+            case "Login":
+                {
+                    //Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_LONG).show();
 
-                final NavController navController = Navigation.findNavController((this), R.id.nav_host_fragment);
-                navController.navigate(R.id.action_viewTutorsFragment_to_loginFragment);
+                    // Two versions of shifting from tutorViewFragment to Loginfragment - couldnt workaround the first one - next one works.
+                    //Navigation.findNavController(findViewById(R.id.MainActivity)).navigate(R.id.action_viewTutorsFragment_to_loginFragment);
+
+                    titleFlag = true;
+                    break;
+                }
+            case "Tutors":
+                {
+                    //NavController navController = Navigation.findNavController((this), R.id.nav_host_fragment);
+
+                    //item.setTitle("Login");
+                    titleFlag = false;
+
+                    //case R.id.action_settings:
+                    break;
+                }
         }
 
+        if(titleFlag) {
+            item.setTitle("Tutors");
+            navController.navigate(R.id.action_viewTutorsFragment_to_loginFragment);
+        }
+        else {
+            item.setTitle("Login");
+            navController.navigate(R.id.action_loginFragment_to_viewTutorsFragment);
+        }
 
         return super.onOptionsItemSelected(item);
+
     }
 
     @Override
@@ -68,6 +99,5 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         //Toast toast = Toast.makeText(getApplicationContext(),currentUser.getDisplayName(),Toast.LENGTH_LONG);
     }
-
 
 }
